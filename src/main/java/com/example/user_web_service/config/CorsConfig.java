49965945db7c509700f3be80a8adf59e703aab8a
Van <vanpqse150505@fpt.edu.vn>
2023-03-11 17:09:53 +0700

    make it better

--- src/main/java/com/example/user_web_service/config/CorsConfig.java
+++ src/main/java/com/example/user_web_service/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
 						HttpMethod.OPTIONS.name(), HttpMethod.DELETE.name())
 				.allowedHeaders("*")
-				.allowedOrigins("http://localhost:8080","https://project-game-rpg.herokuapp.com", "http://localhost:3000");
+				.allowedOrigins("*");
 	}
 
 //	@Bean
