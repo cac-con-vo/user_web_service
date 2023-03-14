ca025103081f2812919d9119bb60af7a6d22fe47
trungnt <trung2kgl@gmail.com>
2023-03-14 22:56:04 +0700

    Update create character for user

--- src/main/java/com/example/user_web_service/controller/CharacterController.java
+++ src/main/java/com/example/user_web_service/controller/CharacterController.java
@@ -1,6 +1,7 @@
 package com.example.user_web_service.controller;
 
 import com.example.user_web_service.dto.ResponseObject;
+import com.example.user_web_service.form.CreateCharacterForm;
 import com.example.user_web_service.form.GameTokenForm;
 import com.example.user_web_service.service.impl.CharacterServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/character")
@@ -24,22 +27,18 @@ public class CharacterController {
     @PostMapping("/createCharacter")
     @SecurityRequirements
     public ResponseEntity<ResponseObject> createCharacter(
-            GameTokenForm gameTokenForm,
-            @Parameter(description = "Input name of character ",example = "concamap") @RequestParam(name = "characterName") String characterName,
-            @Parameter(description = "Input name of game", example = "Dead of souls") @RequestParam(name = "gameName") String gameName,
-            @Parameter(description = "Input name of server", example = "server1") @RequestParam(name = "serverName") String serverName
-
+            @RequestBody @Valid CreateCharacterForm createCharacterForm
     ) {
-        if(gameTokenForm.getGameToken() == null || gameTokenForm.getGameToken().isBlank() || gameTokenForm.getGameToken().isEmpty() ||
-                characterName == null || characterName.isEmpty() || characterName.isBlank() ||
-                serverName == null || serverName.isEmpty() || serverName.isBlank() ||
-                gameName == null || gameName.isEmpty() || gameName.isBlank()){
+        if(createCharacterForm.getGameToken() == null || createCharacterForm.getGameToken().isBlank() || createCharacterForm.getGameToken().isEmpty() ||
+                createCharacterForm.getCharacterName() == null || createCharacterForm.getCharacterName().isEmpty() || createCharacterForm.getCharacterName().isBlank() ||
+                createCharacterForm.getGameName() == null || createCharacterForm.getGameName().isEmpty() || createCharacterForm.getGameName().isBlank() ||
+                createCharacterForm.getCharacterName() == null || createCharacterForm.getCharacterName().isEmpty() || createCharacterForm.getCharacterName().isBlank()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                     new ResponseObject(HttpStatus.BAD_REQUEST.toString(),
                             "Please input data.", null, null)
             );
         }
-        return characterService.creatCharacter(gameTokenForm,characterName.trim(), gameName.trim(), serverName.trim());
+        return characterService.creatCharacter(createCharacterForm);
     }
 
     @Operation(summary = "For get a character for user")
