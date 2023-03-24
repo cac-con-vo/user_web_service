564c06fdc3c75edd0bcf37eccb0b37588eb247e5
Van <vanpqse150505@fpt.edu.vn>
2023-03-24 13:08:30 +0700

    add time play

--- src/main/java/com/example/user_web_service/controller/HomeController.java
+++ src/main/java/com/example/user_web_service/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.example.user_web_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping
+    @Operation(summary = "Home page of web")
+    @GetMapping("/homepages")
     public String index() {
 
         return "Welcome to my app!";
